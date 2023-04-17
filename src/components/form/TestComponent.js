7f03f51551c0c6a7fa1edcdb7057221ee30e8921
ashit-7span <ashit@7span.com>
2023-04-17 17:57:22 +0530

    fix prettier format command

--- src/components/form/TestComponent.js
+++ src/components/form/TestComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
 
 export default function TestComponent() {
-	return <div className="App">Test components55.... adaas</div>;
+	return <div className="App">Test components55.... 
+			add adas adaas</div>;
 }
